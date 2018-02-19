ffcbb4867ab6454d41f1ea4245f6650972c2deaa
randall ishino <rishino24@gmail.com>
2018-02-18 23:55:07 -0800

    delete

--- word.js
+++ word.js
@@ -1,6 +1,18 @@
 var Letter = require("./letter.js");
 
 
-var Word = function() {
+var Word = function(word) {
+    this.word = word;
+    this.lettersGuessed = [];
 
-}
+    this.getLetters = function() {
+        for (var i = 0; i < this.word.length; i++) {
+            var grabbedLetter = new Letter(this.word[i]);
+            this.lettersGuessed.push(grabbedLetter);
+        }
+    };
+
+
+
+
+};
