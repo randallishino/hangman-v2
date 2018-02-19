c8af91d02ff713e8cffe0bf91c5d99bd5bc9239b
randall ishino <rishino24@gmail.com>
2018-02-18 20:48:44 -0800

    inquirer

---
+++ index.js
@@ -0,0 +1,16 @@
+var words = ["Lakers",
+"Warriors",
+"Clippers",
+"Cavaliers",
+"Thunder",
+"Raptors",
+"Wizards",
+"Celtics",
+"Spurs",
+"Sixers",
+"Suns",
+"Jazz",
+"Pelicans"
+];
+
+var inquirer = require("inquirer");
